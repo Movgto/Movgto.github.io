dc27fd509dfae05c4c2e8d6d958a5574fe6d3b4a
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-07-01 15:11:27 -0600

    Fix work cards dinamically generated

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,13 +1,63 @@
-const project = {
-  name: 'Keeping track of hundreds of components',
-  tech: ['Ruby on Rails', 'CSS', 'JavaScript'],
-  desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s."
+const worksCtr = document.getElementById('works-ctnr');
+
+const projectsData = {
+  projects: [{
+    name: 'Keeping track of hundreds of components',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when 
+            an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it 1960s.`
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when 
+            an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it 1960s.`
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when 
+            an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it 1960s.`
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when 
+            an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it 1960s.`
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when 
+            an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it 1960s.`
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when 
+            an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the 
+            printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it 1960s.`
+  }]
 };
 
-let popupAlive = false;
-const htmlElement = document.querySelector('html');
-
-function projectPopUp() {
+function projectPopUp(projectIndex) {
   htmlElement.style.overflowY = 'hidden';
   popupAlive = true;
   const popUpCtr = document.createElement('div');
@@ -19,10 +69,10 @@ function projectPopUp() {
                             <img id='close-popup-btn' src='./icons/popup-cancel.svg' alt='Close popup button' />
                         </div>
                         <div id='popup-name-ctr'>
-                            <h2 id='work-name'>${project.name}</h2>
+                            <h2 id='work-name'>${projectsData.projects[projectIndex].name}</h2>
                         </div>
                         <ul class='tech' id='skill-popup-ctr'></ul>
-                        <p id='work-desc'>${project.desc}</p>
+                        <p id='work-desc'>${projectsData.projects[projectIndex].desc}</p>
                         <div id='btn-popup-ctr'>
                             <button class='see-btn' type='button'>See live <img src='./icons/see-live.svg' /></button>
                             <button class='see-btn' type='button'>See source <img src='./icons/see-source.svg' /></button>
@@ -57,13 +107,46 @@ function projectPopUp() {
     }, 200);
   });
 
-  project.tech.forEach((item) => {
+  projectsData.projects[projectIndex].tech.forEach((item) => {
     const skillCard = document.createElement('li');
     skillCard.setAttribute('class', 'tech-card popup');
     skillCard.innerHTML = `<p>${item}</p>`;
     skillCtr.appendChild(skillCard);
   });
 }
+
+projectsData.projects.forEach((item, i) => {
+  const workCard = document.createElement('div');
+  workCard.className = `work-card ${i}`;
+  workCard.innerHTML = `
+                        <div class="bottom-area">
+                            <h2 class="top">
+                                ${item.name}
+                            </h2>
+                            <ul class="tech">
+                            </ul>
+                            <div class="bottom">
+                                <button class="see-btn" type="button">
+                                    See Project
+                                </button>
+                            </div>
+                        </div>
+                        `;
+  const tech = workCard.querySelector(`.tech`);
+  const seeBtn = workCard.querySelector('.see-btn');
+  projectsData.projects[i].tech.forEach((item) => {
+    const techCard = document.createElement('li');
+    techCard.className = 'tech-card';
+    techCard.innerHTML = `<p>${item}</p>`;
+    tech.appendChild(techCard);
+  });
+
+  worksCtr.appendChild(workCard);
+  seeBtn.addEventListener('click', () => projectPopUp(i));
+});
+
+let popupAlive = false;
+const htmlElement = document.querySelector('html');
 
 window.addEventListener('resize', () => {
   if (window.innerWidth > 765 && popupAlive) {
@@ -81,10 +164,4 @@ window.addEventListener('resize', () => {
     popupImg.setAttribute('src', './icons/cancel.svg');
     popupCtr.appendChild(buttonCtr);
   }
-});
-
-const seeBtns = document.querySelectorAll('.work-card button');
-
-seeBtns.forEach((items) => {
-  items.addEventListener('click', projectPopUp);
 });
